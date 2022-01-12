a185030db187f7a9959a3e2554177dc2ea4cc5e8
derek@derekerb.solutions <Gandalf*42!>
2022-01-12 11:58:39 +0100

    V1.0.28b STYLE: (html) Tweak SM meta tags and images

--- calctvasw.js
+++ calctvasw.js
@@ -9,7 +9,7 @@
  ********************************************************************/
 
 // Local all app file assets to be loaded to the precache
-const cacheName = 'calctva-cache-v1.0.27b ';
+const cacheName = 'calctva-cache-v1.0.28b ';
 
 /*************************************************************************
  ** EVENT LISTENER
